c484cf809c8ca34d262b6739672f2d8cb587a006
Sebika Nepal <sebikanepal4@gmail.com>
2023-06-12 22:06:04 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //working on variables
+// $firstname="Sebika";
+// $lastname="nepal";
+// $age=23;
+// $weight=53;
+// echo " My name is".$firstname."".$lastname." age=".$age." and ".$weight."with wweight<br>";
+
+// $num1=20;
+// $num2=30;
+// $sum=$num1+$num2;
+// echo var_dump($name);
+// echo "the sum is".$sum."<br>";
+
+//constants working demo
+// define("name","Sebika_nepal");
+// echo name;
+// if($age<18){
+//     echo"not elligible<br>";
+// ;
+// }
+// else{
+//     echo"elligible to vote<br>";
+
+// }
+
+// while($sum<=60){
+//     echo $sum."<br>";
+//     $sum++;
+
+// }
+//array
+// $names=array("sebika","rojina","anbika");
+// foreach($names as $values){
+//     echo $values."<br>";
+// }
+//function in php
+// function numbers(){
+//     global $num1,$num2;
+//     echo $num1."<br>".$num2;
+// } 
+// numbers();
+
+//making contact details
+
+
+?>
+<header><h1>My Contact Applications</h1></header>
+</body>
+</html>
